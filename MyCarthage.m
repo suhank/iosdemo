f5ee3962f53d80bd1bd4acbaa6f600975afb7c14
mac <mac@macdeMacBook-Pro.local>
2016-10-20 17:44:42 +0800

    m file

---
+++ MyCarthage.m
@@ -0,0 +1,16 @@
+//
+//  MyCarthage.m
+//  testdemo1
+//
+//  Created by mac on 16/10/20.
+//  Copyright (c) 2016年 r2. All rights reserved.
+//
+
+#import "MyCarthage.h"
+
+@implementation MyCarthage
+-(NSString *) getCarthageSkill
+{
+    return @"HI WORLD";
+}
+@end
