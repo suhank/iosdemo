af218fbb3acd9f27b9a6c4d937cc8c0a57333465
mac <mac@macdeMacBook-Pro.local>
2016-10-20 17:30:25 +0800

    first commit

---
+++ MyCarthage.h
@@ -0,0 +1,17 @@
+//
+//  MyCarthage.h
+//  testdemo1
+//
+//  Created by mac on 16/10/20.
+//  Copyright (c) 2016年 r2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyCarthage : NSObject
+{
+    
+}
+
+-(NSString *) getCarthageSkill;
+@end
